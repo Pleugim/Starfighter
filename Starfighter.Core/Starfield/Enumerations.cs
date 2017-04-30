80649df1435cc5f8d955034aa46b19c137ff1ffc
Pleugim <miguelpeterson@gmail.com>
2017-04-30 16:47:16 -0500

    Initial Commit
    The first commit.

---
+++ Starfighter.Core/Starfield/Enumerations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starfighter.Core.Starfield
+{
+    public enum RelativePosition
+    {
+        Top = 0,
+        TopRight = 1,
+        Right = 2,
+        BottomRight = 3,
+        Bottom = 4,
+        BottomLeft = 5,
+        Left = 6,
+        TopLeft  = 7
+    }
+
+    public enum Position
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7
+    }
+}
---
+++ Starfighter.Core/Starfield/StarNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starfighter.Core.Starfield
+{
+    public class StarNode
+    {
+        public const int Directions = 8;
+
+        public int Id { get; set; }
+        public List<StarNode> Adjacents = new List<StarNode>();
+
+        public StarNode GetAdjacent(int position)
+        {
+            position = position % Directions;
+
+            if (position < this.Adjacents.Count)
+            {
+                return this.Adjacents[position];
+            }
+
+            return null;
+        }
+
+    }
+}
